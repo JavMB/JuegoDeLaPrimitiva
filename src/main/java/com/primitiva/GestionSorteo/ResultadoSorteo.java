c674b6cd54d577edd04e6383066f2fc33f097cd3
Javi <javiermengual@live.com>
2025-03-06 22:11:19 +0100

    elimino los setters voy a hacerlo inmutable , siguiendo otro enfoque

--- src/main/java/com/primitiva/GestionSorteo/ResultadoSorteo.java
+++ src/main/java/com/primitiva/GestionSorteo/ResultadoSorteo.java
@@ -1,6 +1,6 @@
 package com.primitiva.GestionSorteo;
 
-import java.time.LocalDateTime;
+
 import java.util.Arrays;
 
 /**
@@ -11,7 +11,7 @@ public class ResultadoSorteo {
     private int[] combinacion;
     private int complementario;
     private int reintegro;
-    private LocalDateTime fechaHora;
+
 
     /**
      * Constructor de la clase {@code ResultadoSorteo}.
@@ -25,14 +25,13 @@ public class ResultadoSorteo {
         this.combinacion = Arrays.copyOf(combinacion, combinacion.length);
         this.complementario = complementario;
         this.reintegro = reintegro;
-        this.fechaHora = LocalDateTime.now();
     }
 
     /**
      * Obtiene la combinación ganadora del sorteo.
      *
      * @return Un array de enteros que representa la combinación ganadora. Se retorna una copia
-     *         defensiva para evitar modificaciones externas.
+     * defensiva para evitar modificaciones externas.
      */
     public int[] getCombinacion() {
         return Arrays.copyOf(combinacion, combinacion.length);
@@ -56,49 +55,5 @@ public class ResultadoSorteo {
         return reintegro;
     }
 
-    /**
-     * Obtiene la fecha y hora en que se realizó el sorteo.
-     *
-     * @return La fecha y hora del sorteo.
-     */
-    public LocalDateTime getFechaHora() {
-        return fechaHora;
-    }
 
-    /**
-     * Establece la combinación ganadora del sorteo.
-     *
-     * @param combinacion El array de enteros que representa la nueva combinación ganadora.
-     *                    Se realiza una copia defensiva para evitar modificaciones externas.
-     */
-    void setCombinacion(int[] combinacion) {
-        this.combinacion = Arrays.copyOf(combinacion, combinacion.length);
-    }
-
-    /**
-     * Establece el número complementario del sorteo.
-     *
-     * @param complementario El nuevo número complementario.
-     */
-    void setComplementario(int complementario) {
-        this.complementario = complementario;
-    }
-
-    /**
-     * Establece el número de reintegro del sorteo.
-     *
-     * @param reintegro El nuevo número de reintegro.
-     */
-    void setReintegro(int reintegro) {
-        this.reintegro = reintegro;
-    }
-
-    /**
-     * Establece la fecha y hora en que se realizó el sorteo.
-     *
-     * @param fechaHora La nueva fecha y hora del sorteo.
-     */
-    void setFechaHora(LocalDateTime fechaHora) {
-        this.fechaHora = fechaHora;
-    }
 }
